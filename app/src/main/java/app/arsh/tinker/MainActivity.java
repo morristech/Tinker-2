f5073d8aca16a3ddfba138f6aad52f6bf13d682d
Arash Khangaldi <khangaldi.arash@gmail.com>
2016-12-01 08:54:54 +0330

    Extracting CountryServie to a Presenter

--- app/src/main/java/app/arsh/tinker/MainActivity.java
+++ app/src/main/java/app/arsh/tinker/MainActivity.java
@@ -3,6 +3,7 @@ package app.arsh.tinker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import app.arsh.tinker.presenter.MainActivityPresenter;
 import java.util.List;
 import javax.inject.Inject;
 import retrofit2.Call;
@@ -12,22 +13,11 @@ import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Inject Call<List<Country>> countryList;
-
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    ((TinkerApp) getApplication()).getNetComponent().inject(this);
+    new MainActivityPresenter(getApplication());
 
-    countryList.enqueue(new Callback<List<Country>>() {
-      @Override public void onResponse(Call<List<Country>> call, Response<List<Country>> response) {
-        Log.v("Tinker_", response.body().toString());
-      }
-
-      @Override public void onFailure(Call<List<Country>> call, Throwable t) {
-        Log.v("Tinker_", t.getLocalizedMessage());
-      }
-    });
   }
 }
